bb433e5fc9ace885aef3c1fc01fbe0c92809f5bf
yyhsz <591985864@qq.com>
2019-03-04 21:59:47 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import x from './module1'
+import y from './module2'
+x()
+y()
+console.log('lajiwebpack')
+import './default.scss' 
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const cssnext = require('postcss-cssnext');
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+        ],
+    },
+
+};
